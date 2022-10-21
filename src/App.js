36c671052a093ba5485144a6d4a3505c5e420edf
Maksym.info <nagornymaxim@gmail.com>
2022-10-21 13:23:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+const gratingStyle = {
+    border: "2px solid #000",
+    maxWidth: "300px",
+    textAlign: "center",
+    margin: "10px auto"
+}
+const gratingsList = [
+    {text: 'Hello 1'},
+    {text: 'Hello 2'},
+    {text: 'Hello 3'}
+];
+
+function Gratings(props) {
+    return (
+        <div style={gratingStyle}>
+            {props.element}
+        </div>
+    )
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            {gratingsList.map((item, index) => <Gratings
+                element = {item.text}
+                key = {index}
+            />)}
+        </>
+    );
 }
 
 export default App;
